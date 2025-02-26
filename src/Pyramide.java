cf3adf272acd5455165f52be781379fdf1d9967e
gleni <glesel19@htl-shkoder.com>
2025-02-26 08:06:27 +0100

    abstract class pyramide

--- src/Pyramide.java
+++ src/Pyramide.java
@@ -1,3 +1,17 @@
 public abstract class Pyramide {
-}
+    protected double hoehe;
 
+    public Pyramide(double hoehe) {
+        this.hoehe = hoehe;
+    }
+
+    public double getHoehe() {
+        return hoehe;
+    }
+
+    public void setHoehe(double hoehe) {
+        this.hoehe = hoehe;
+    }
+
+    public abstract double calcVolumen();
+}
